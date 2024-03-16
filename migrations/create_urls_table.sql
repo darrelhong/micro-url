7ac07e000227d082160f2ec2e7878d8326dbf7b0
darrelhong <darrel.hong@gmail.com>
2024-03-16 16:29:52 +0800

    feat: add index on short_url_id for query

--- migrations/create_urls_table.sql
+++ migrations/create_urls_table.sql
@@ -1,6 +1,8 @@
-CREATE TABLE urls (
+CREATE TABLE IF NOT EXISTS urls (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     original_url TEXT NOT NULL,
     short_url_id TEXT NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE INDEX idx_urls_short_url_id ON urls(short_url_id);
